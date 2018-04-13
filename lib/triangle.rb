69897b9e692b9891a6a8adb3cdb9ca0496d5818f
Luis Rodriguez <zacha@tutanota.com>
2018-04-13 23:02:15 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
       raise TriangleError
     end
 
-    if @side1 + @side2 < @side3 || @side1 + @side3 < @side2 || @side2 + @side3 < @side1
+    if @side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 || @side2 + @side3 <= @side1
       raise TriangleError
     end
 
