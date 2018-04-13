30f2da15cd83153cd0abced2ed6b91529392782d
Luis Rodriguez <zacha@tutanota.com>
2018-04-13 23:01:34 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,10 @@ class Triangle
       raise TriangleError
     end
 
+    if @side1 + @side2 < @side3 || @side1 + @side3 < @side2 || @side2 + @side3 < @side1
+      raise TriangleError
+    end
+
     if @side1 == @side2 && @side1 == @side3
       return :equilateral
     elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
